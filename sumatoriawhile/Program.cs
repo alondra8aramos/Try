de670fbd2d044ba4082d61337532322ae327ab9b
Alondra <alondra8aramos@gmail.com>
2021-12-07 09:38:33 -0700

    Add files via upload

---
+++ sumatoriawhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumatoriawhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            Console.WriteLine("Escriba el numero para sumatoria");
+            Num = Convert.ToInt32(Console.ReadLine());
+            Num = Num * (Num + 1) / 2;
+            Console.WriteLine(Num);
+            Console.ReadLine();
+        }
+    }
+}
